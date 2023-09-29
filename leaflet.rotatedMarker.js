fdda629734aa23fbae823cb024b27b1d1d80416a
Njål Gjermundshaug <njal@degree.no>
2023-09-29 12:12:49 +0200

    Update leaflet.rotatedMarker.js
    Rotate in correct direction in order to reach desired angle with minimum rotation desired

--- leaflet.rotatedMarker.js
+++ leaflet.rotatedMarker.js
@@ -4,6 +4,7 @@
     var proto_setPos = L.Marker.prototype._setPos;
 
     var oldIE = (L.DomUtil.TRANSFORM === 'msTransform');
+    var rot = 0;
 
     L.Marker.addInitHook(function () {
         var iconOptions = this.options.icon && this.options.icon.options;
@@ -34,12 +35,23 @@
             if(this.options.rotationAngle) {
                 this._icon.style[L.DomUtil.TRANSFORM+'Origin'] = this.options.rotationOrigin;
 
+                // Calculate closest rotation angle (clockwise or counterclockwise)
+                // https://stackoverflow.com/questions/19618745/css3-rotate-transition-doesnt-take-shortest-way
+                var nR = this.options.rotationAngle;
+                var aR;
+                this.rot = this.rot || 0; // if rot undefined or 0, make 0, else rot
+                aR = this.rot % 360;
+                if ( aR < 0 ) { aR += 360; }
+                if ( aR < 180 && (nR > (aR + 180)) ) { this.rot -= 360; }
+                if ( aR >= 180 && (nR <= (aR - 180)) ) { this.rot += 360; }
+                this.rot += (nR - aR);
+        
                 if(oldIE || !this.options.accelerated) {
                     // for IE 9, use the 2D rotation
-                    this._icon.style[L.DomUtil.TRANSFORM] = 'rotate(' + this.options.rotationAngle + 'deg)';
+                    this._icon.style[L.DomUtil.TRANSFORM] = 'rotate(' + this.rot + 'deg)';
                 } else {
                     // for modern browsers, prefer the 3D accelerated version
-                    this._icon.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.options.rotationAngle + 'deg)';
+                    this._icon.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.rot + 'deg)';
                 }
             }
         },
